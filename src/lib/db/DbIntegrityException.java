ae110259fcf02dd9482a0d0a04c0b27a85503291
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-11-24 12:56:23 -0300

    Primeiro commit

---
+++ src/lib/db/DbIntegrityException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class DbIntegrityException extends RuntimeException{
+    private static final long serialVersionUID = 1l;
+
+    public DbIntegrityException(String msg){
+        super(msg);
+    }
+    
+}
