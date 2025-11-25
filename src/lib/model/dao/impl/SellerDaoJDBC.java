b2a03a0bdccce1710530b0ff54b26d901303b1b3
Vitória Leticia da Silva Medeiros <vitoria.medeiros@invillia.com>
2025-11-25 12:59:03 -0300

    Reusing instantion

--- src/lib/model/dao/impl/SellerDaoJDBC.java
+++ src/lib/model/dao/impl/SellerDaoJDBC.java
@@ -51,16 +51,8 @@ public SellerDaoJDBC(Connection conn){
             st.setInt(1,id);
             rs = st.executeQuery();
             if (rs.next()) {
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setBaseSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthDate(rs.getDate("BirthDate"));
-                obj.setDepartment(dep);
+                Department dep = instantiateDepartment(rs);
+                Seller obj = instantiateSeller(rs,dep);
                 return obj;
 
             } 
@@ -73,6 +65,25 @@ public SellerDaoJDBC(Connection conn){
             DB.closeResultSet(rs);
         }
     }
+
+    private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+    Seller obj = new Seller();
+    obj.setId(rs.getInt("Id"));
+    obj.setName(rs.getString("Name"));
+    obj.setEmail(rs.getString("Email"));
+    obj.setBaseSalary(rs.getDouble("BaseSalary"));
+    obj.setBirthDate(rs.getDate("BirthDate"));
+    obj.setDepartment(dep);
+    return obj;
+    }
+   
+    private Department instantiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString ("DepName"));
+        return dep;
+    }
+
 
     public Seller findById(Integer id) {
         // TODO Auto-generated method stub
